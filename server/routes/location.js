0c0775f9a69f92217345f40364959bee3bd05738
SuhWanggyu <wang1@hanyang.ac.kr>
2020-09-20 20:55:20 +0900

    Add swagger document and fix template

--- server/routes/location.js
+++ server/routes/location.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET all location */
 router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+/* GET all location of specific district */
+router.get('/:districtName', function(req, res, next) {
   res.send('respond with a resource');
 });
 
